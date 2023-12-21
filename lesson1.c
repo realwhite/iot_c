c586dc51873fcaea89b91e9bfb425c1aa8ab1281
Andrey Beliy <andreybeliy@Andreys-MacBook-Pro.local>
2023-12-21 22:31:39 +0300

    first lesson

---
+++ lesson1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello world!\n\n");
+    printf("Let's\n"
+           "   go\n"
+           "     to walk\n\n");
+
+    printf("   *   \n"
+           "  ***  \n"
+           " ***** \n"
+           "*******\n"
+           " HH HH \n"
+           " ZZZZZ \n");
+
+    return 0;
+}
